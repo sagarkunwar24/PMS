4be2e8e3004be0cfb58e2e30ed025e84a8c62a99
cameron-yee <yee.camero23@gmail.com>
2018-02-10 01:41:55 -0700

    Changed DATABASES library to include mysql

--- pms/pms/settings.py
+++ pms/pms/settings.py
@@ -25,7 +25,9 @@ SECRET_KEY = '#uiby*ma-#1)ww84sdke!ov!&jh3fvsenvl)nvkceosju$o%(+'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '127.0.0.1',
+]
 
 
 # Application definition
@@ -74,9 +76,18 @@ WSGI_APPLICATION = 'pms.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+    #TODO: Switch to mysql db
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'pms',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
+        'PORT': '3306',
     }
 }
 
